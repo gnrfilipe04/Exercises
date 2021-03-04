486c09ac8cbc3333cb43d4c85a57650fe66323a4
gnrfilipe04 <gnr_filipe@hotmail.com>
2021-03-04 20:21:39 -0300

    add exercise 2

---
+++ Exercise2.js
@@ -0,0 +1,8 @@
+// Write a function that takes a number as argument
+// If the number is even, return true
+// Otherwise, return false
+
+function myFunction(number) {
+    return number % 2 === 0 ? true : false
+ }
+ 
