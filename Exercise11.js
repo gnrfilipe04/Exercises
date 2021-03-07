e3fa2dbade048d15dab056b0f0d0d0c1f12dbd01
gnrfilipe04 <gnr_filipe@hotmail.com>
2021-03-07 20:11:00 -0300

    add exercise 11 - Round a number to 2 decimal places

---
+++ Exercise11.js
@@ -0,0 +1,7 @@
+// Write a function that takes a number as argument
+// Round the number to the 2nd digit after the comma
+// Return the rounded number
+
+function myFunction(num) {
+  return Number(num.toFixed(2));
+}
