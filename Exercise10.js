432142229384fd8c26efec995df880d97a8661f8
gnrfilipe04 <gnr_filipe@hotmail.com>
2021-03-07 19:54:34 -0300

    add exercise 10 - Split a number into its digits

---
+++ Exercise10.js
@@ -0,0 +1,9 @@
+// Write a function that takes a number as argument
+// The function should split the number into its individual digits and return them in an array
+// Tipp: you might want to change the type of the number for the splitting
+
+function myFunction( num ) {
+    const string = String(num);
+    const strings = string.split('');
+    return strings.map(digit => Number(digit));
+}
